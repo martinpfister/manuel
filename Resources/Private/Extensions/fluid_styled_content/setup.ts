d4a37209fe37db221b6665fadef18bab4921396b
Manuel Glauser <manuel.glauser@staempfli.com>
2015-11-13 15:06:35 +0100

    Implement responsive tables (add GetChildViewHelper).

--- Resources/Private/Extensions/fluid_styled_content/setup.ts
+++ Resources/Private/Extensions/fluid_styled_content/setup.ts
@@ -1,5 +1,5 @@
 lib.fluidContent {
-    templateRootPaths.10 = EXT:{$plugin.templatebootstrap.packageKey}/Resources/Private/Extensions/fluid_styled_content/Templates/
+    templateRootPaths.10 = EXT:{$plugin.templatebootstrap.packageKey}/Resources/Private/Extensions/fluid_styled_content/Templates/Content/
     layoutRootPaths.10 = EXT:{$plugin.templatebootstrap.packageKey}/Resources/Private/Extensions/fluid_styled_content/Layouts/
     partialRootPaths.10 = EXT:{$plugin.templatebootstrap.packageKey}/Resources/Private/Extensions/fluid_styled_content/Partials/
 }
