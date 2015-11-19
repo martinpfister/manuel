b4f09e7bfc78980dccda3ad7203d809c73f0b190
Manuel Glauser <manuel.glauser@staempfli.com>
2015-11-19 14:57:03 +0100

    Remove obsolete tt_content TCA override as soundcloud files are centrally registered in ext_localconf > mediafile_ext.

--- Configuration/TCA/Overrides/tt_content.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('TYPO3_MODE') or die();
-$GLOBALS['TCA']['tt_content']['columns']['assets']['config']['filter'][0]['parameters']['allowedFileExtensions'] .= ',soundcloud';
-$GLOBALS['TCA']['tt_content']['columns']['assets']['config']['foreign_selector_fieldTcaOverride']['config']['appearance']['elementBrowserAllowed'] .= ',soundcloud';
